201a5b7568d281bef12f1c8a0cb0f362b2176b44
deb <deb@dlat>
2017-01-31 14:43:42 -0500

    getbits, printbits: consistently use unsigned type
    Also, getbits() correctly accepts p==0

--- getbits.c
+++ getbits.c
@@ -4,9 +4,9 @@
 /**
  * n bit fields of x, starting at position p
  * right-shifted */
-int getbits(unsigned x, int p, int n)
+unsigned getbits(unsigned x, int p, int n)
 {
-	assert( p > 0 && n > 0 && p+1-n >= 0 );
+	assert( p >= 0 && n > 0 && p+1-n >= 0 );
 	return ( x >> (p+1-n) & ~(~0 << n) );
 }
 
--- printbits.c
+++ printbits.c
@@ -5,7 +5,7 @@
  * print binary representation of the n least
  * significant bits of x. 
  */
-void printbits(long x, unsigned n)
+void printbits(unsigned x, int n)
 {
 	for (; n>0; n--)
 		putchar(x & 1 << n-1 ? '1' : '0');
